854064d3a387ca00d7b79cc2d112b3ecc924f51b
Sanket01-debug <sanketkansal2001@gmail.com>
2026-08-02 13:13:46 +0530

    feat: add Connections and Requests components with routing

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,26 @@ import Profile from "./components/Profile"
 import { Provider } from "react-redux"
 import appStore from "./utils/appStore"
 import Feed from "./components/Feed"
+import Connections from "./components/Connections";
+import Requests from "./components/Requests";
 
 function App() {
   return (
     <>
-    <Provider store={appStore}>
-    <BrowserRouter basename="/">
-    <Routes>
-      <Route path="/" element={<Body />}>
-      <Route path="/" element={<Feed />}/>
-      <Route path="/login" element={<Login />}/>
-      <Route path="/profile" element={<Profile />}/>
-      </Route>
-    </Routes>
-    </BrowserRouter>
-    </Provider>
-    {/* <NavBar/>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/" element={<Feed />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/connections" element={<Connections />} />
+              <Route path="/requests" element={<Requests />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+      {/* <NavBar/>
     <h1 className="text-3xl font-bold ">Hello World!</h1> */}
     </>
   )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -48,7 +48,7 @@ const NavBar = () => {
                                     <span className="badge">New</span>
                                 </Link>
                             </li>
-                            <li><a>Settings</a></li>
+                            <li><a>Connections</a></li>
                             <li>
                                 <a onClick={handleLogout}>Logout</a>
                             </li>
